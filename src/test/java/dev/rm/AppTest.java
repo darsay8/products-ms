c20b066389ce823f89eb2188519a24c5460980f2
darsay8 <darsay.one@gmail.com>
2024-11-17 23:05:21 -0300

    Add initial project structure using maven archetype ⚡️

---
+++ src/test/java/dev/rm/AppTest.java
@@ -0,0 +1,14 @@
+package dev.rm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Test if the context loads
+    }
+
+}
